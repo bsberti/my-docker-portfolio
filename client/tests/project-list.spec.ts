b23d847bab023f3690337e42c784292caf07291c
Bruno Berti <bsberti@gmail.com>
2025-08-04 20:53:22 -0300

    Ajuste no ci.yml para requisição HTTP GET para endpoint da API

--- client/tests/project-list.spec.ts
+++ client/tests/project-list.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('Interface deve listar os projetos na tela inicial', async ({ page }) => {
-  await page.goto('http://localhost:3000'); // ajuste se a porta for diferente
+  await page.goto('http://127.0.0.1:3000'); // ajuste se a porta for diferente
 
   const projectItems = page.locator('li'); // ou outro seletor do ProjectList
   await expect(projectItems.first()).toBeVisible();
--- server/tests/projects-api.spec.ts
+++ server/tests/projects-api.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('GET /api/projects deve retornar lista de projetos', async ({ request }) => {
-  const response = await request.get('http://localhost:5000/api/projects'); // Ajuste a porta se for diferente
+  const response = await request.get('http://127.0.0.1:5000/api/projects'); // Ajuste a porta se for diferente
   expect(response.ok()).toBeTruthy();
 
   const data = await response.json();
