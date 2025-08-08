afdfda67952222d105824bb861cba63ae11a20f7
Bruno Berti <bsberti@gmail.com>
2025-08-08 15:10:47 -0300

    Correção no teste do GitHub Actions

--- client/tests/project-list.spec.ts
+++ client/tests/project-list.spec.ts
@@ -3,14 +3,23 @@ import { test, expect } from '@playwright/test';
 test('Interface deve listar os projetos na tela inicial', async ({ page }) => {
   await page.route('**/projects', route => route.continue());
   await page.goto('http://127.0.0.1:3000');
+  
+  page.on('request', req => {
+    if (req.url().includes('/projects')) {
+      console.log('[Playwright] Chamando:', req.url());
+    }
+  });
 
   await page.waitForResponse(resp =>
-    resp.url().includes('/projects') && resp.status() === 200
+    resp.url().includes('/api/projects') && resp.status() === 200
   );
 
   const projectItems = page.locator('[data-testid="project-item"]');
-  await expect(projectItems.first()).toBeVisible();
+  //await expect(projectItems.first()).toBeVisible();
 
   const count = await projectItems.count();
   console.log('Total de projetos encontrados:', count);
+
+  // Falha se não encontrou nenhum
+  expect(count).toBeGreaterThan(0);
 });
